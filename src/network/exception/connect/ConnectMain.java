87b10c5e5d1c30c56ed4ebc90d52c68f57b98b51
dohun <shine_17@naver.com>
2025-10-02 21:01:28 +0900

    add section 9: Network connection exception, timeout, resources

---
+++ src/network/exception/connect/ConnectMain.java
@@ -0,0 +1,39 @@
+package network.exception.connect;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ConnectMain {
+
+    public static void main(String[] args) throws IOException {
+        unknownHostEx1();
+        unknownHostEx2();
+        connectionRefused(); // Connection refused: 서버는 연결됐지만 PORT 연결이 되지 않았을 때
+    }
+
+    private static void unknownHostEx1() throws IOException {
+        try {
+            Socket socket = new Socket("999.999.999.999", 80);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void unknownHostEx2() throws IOException {
+        try {
+            Socket socket = new Socket("google.gogo", 80);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void connectionRefused() throws IOException {
+        try {
+            Socket socket = new Socket("localhost", 45678);
+        } catch (ConnectException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
